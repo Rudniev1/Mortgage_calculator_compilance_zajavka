faa6919bf7b4ac9ea4393ebfb30d5aab06305f84
Rudniev1 <shqdnix1112@gmail.com>
2024-02-21 21:24:55 +0100

    Feature 001: upgrade to build.gradle

---
+++ src/main/java/com/company/configuration/CalculatorConfiguration.java
@@ -0,0 +1,9 @@
+package com.company.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "pl.zajavka")
+public class CalculatorConfiguration {
+}
